744ec4a13536dde210dc4e0c028922c929107748
Radi D <spacex13@abv.bg>
2018-12-24 13:23:10 +0200

    styled quote create form view, etc.

--- Honeypot/ViewModels/Quote/CreateViewModel.cs
+++ Honeypot/ViewModels/Quote/CreateViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Honeypot.ViewModels.Quote
 {
@@ -7,12 +8,15 @@ namespace Honeypot.ViewModels.Quote
         [Required]
         [StringLength(1000, ErrorMessage = "Quote length exceeded the maximum!")]
         [DataType(DataType.Text)]
+        [DisplayName("Quote")]
         public string Text { get; set; }
 
         [Required]
+        [DisplayName("Author")]
         public int AuthorId { get; set; }
 
         [Required]
+        [DisplayName("Book")]
         public int BookId { get; set; }
     }
 }
